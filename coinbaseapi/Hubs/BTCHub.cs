a220c41675ed2305f9aef9523c3adfe0c1bb00a5
Andrew Napper <nappa.andrewnapper@gmail.com>
2020-08-10 21:11:10 +1200

    Tidying

--- coinbaseapi/Hubs/BTCHub.cs
+++ coinbaseapi/Hubs/BTCHub.cs
@@ -5,31 +5,31 @@ using Microsoft.Extensions.Caching.Memory;
 
 namespace coinbaseapi.Hubs
 {
-  public class BTCHub : Hub
-  {
-    private readonly IApiService _apiService;
-    private readonly IMemoryCache _memoryCache;
-
-    public BTCHub(IApiService apiService, IMemoryCache memoryCache)
+    public class BTCHub : Hub
     {
-      _apiService = apiService;
-      _memoryCache = memoryCache;
-    }
+        private readonly IApiService _apiService;
+        private readonly IMemoryCache _memoryCache;
 
-    public override async Task OnConnectedAsync()
-    {
-      await InitializeBtcPolling();
-      await base.OnConnectedAsync();
-    }
+        public BTCHub(IApiService apiService, IMemoryCache memoryCache)
+        {
+            _apiService = apiService;
+            _memoryCache = memoryCache;
+        }
 
-    private async Task InitializeBtcPolling()
-    {
-      var priceList = _memoryCache.Get("PriceList");
-      if (priceList == null)
-      {
-        await _apiService.StartPollingCoindesk();
-      }
-    }
+        public override async Task OnConnectedAsync()
+        {
+            await InitializeBtcPolling();
+            await base.OnConnectedAsync();
+        }
 
-  }
+        private async Task InitializeBtcPolling()
+        {
+            var priceList = _memoryCache.Get("PriceList");
+            if (priceList == null)
+            {
+                await _apiService.StartPollingCoindesk();
+            }
+        }
+
+    }
 }
--- coinbaseapi/Models/Price.cs
+++ coinbaseapi/Models/Price.cs
@@ -2,9 +2,9 @@ using System;
 
 namespace coinbaseapi.Models
 {
-  public class Price
-  {
-    public float Value { get; set; }
-    public DateTime Date { get; set; }
-  }
+    public class Price
+    {
+        public float Value { get; set; }
+        public DateTime Date { get; set; }
+    }
 }
